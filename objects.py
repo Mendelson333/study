b717a9d00c00e593c80e31e503c8bb1f7fc8a466
Mendelson333 <wtfekin@gmail.com>
2024-03-04 00:10:54 +0400

    namespace

---
+++ objects.py
@@ -0,0 +1,9 @@
+class House:
+    def __init__(self):
+        self.NumberOfFloors = "10"
+
+house = House()
+for i in house.NumberOfFloors:
+    print("Текущий этаж равен "+ house.NumberOfFloors)
+
+
